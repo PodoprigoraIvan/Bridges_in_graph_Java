18087b2ef9e168a87ca99ecdbf7b7fe5aed97c79
Mixagit <mixa.penkin@gmail.com>
2022-07-02 22:42:50 +0300

    alg: done

--- src/main/java/algorithm/Algorithm.java
+++ src/main/java/algorithm/Algorithm.java
@@ -1,4 +1,5 @@
-package bridges;
+package algorithm;
+
 import graphcomponents.*;
 import java.util.ArrayList;
 
@@ -45,32 +46,12 @@ public class Application {
         }
     }
 
-    public static ArrayList<Edge> findBridges(ArrayList<Vertex> vertex_list, ArrayList<Edge> edges_list){ArrayList<ArrayList<Integer>> graph = converter(vertex_list, edges_list);
+    public static ArrayList<Edge> findBridges(ArrayList<Vertex> vertex_list, ArrayList<Edge> edges_list){
+		ArrayList<ArrayList<Integer>> graph = converter(vertex_list, edges_list);
         ArrayList<Edge> bridges_list = new ArrayList<>();
         for (int i = 0; i < graph.size(); ++i)
             if (used[i] == false)
                 dfs(i, -1, graph, vertex_list, bridges_list);
         return bridges_list;
-    }
-
-    public static void main( String[] args ) {
-        ArrayList<Vertex> vertex_list = new ArrayList<>();
-        ArrayList<Edge> edges_list = new ArrayList<>();
-        Vertex v0 = new Vertex(100, 100, 0);
-        Vertex v1 = new Vertex(100, 50, 1);
-        Vertex v2 = new Vertex(50, 100, 2);
-        vertex_list.add(v0);
-        vertex_list.add(v1);
-        vertex_list.add(v2);
-        edges_list.add(new Edge(v0, v1));
-        edges_list.add(new Edge(v1, v2));
-
-
-        ArrayList<Edge> bridges_list = findBridges(vertex_list, edges_list);
-
-
-        for (Edge e : bridges_list){
-            System.out.printf("%d %d   ", e.first.num, e.second.num);
-        }
     }
 }
