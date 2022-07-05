b426f14f5f3eae91bffa0897453beec0c1f79ab3
Mixagit <mixa.penkin@gmail.com>
2022-07-05 11:02:35 +0300

    alg: redone 2

--- src/main/java/com/github/podoprigoraivan/swingapp/algorithm/BridgesFinder.java
+++ src/main/java/com/github/podoprigoraivan/swingapp/algorithm/BridgesFinder.java
@@ -1,60 +1,48 @@
-package com.github.podoprigoraivan.swingapp.algorithm;
+package algorithm;
 
-import com.github.podoprigoraivan.swingapp.graph.*;
+import graph.*;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class BridgesFinder {
-    public static int maxN = 1000;
-    public static boolean[] used = new boolean[maxN];
-    public static int[] tin = new int[maxN];
-    public static int[] fup = new int[maxN];
-    public static int timer = 0;
+    private static boolean[] used;
+    private static int[] tin;
+    private static int[] fup;
+    private static int timer;
 
-    public static ArrayList<ArrayList<Integer>> converter(ArrayList<Vertex> vertex_list, ArrayList<Edge> edges_list) {
-        ArrayList<ArrayList<Integer>> graph = new ArrayList<>(vertex_list.size()+1);
-        for(int i = 0; i < vertex_list.size()+1; i++) {
-            graph.add(new ArrayList());
-        }
-        for(Edge edge: edges_list){
-            graph.get(edge.first.num).add(edge.second.num);
-            graph.get(edge.second.num).add(edge.first.num);
-        }
-        return graph;
+    public BridgesFinder(Graph graph) {
+        used = new boolean[graph.VertexAmount()];
+        tin = new int[graph.VertexAmount()];
+        fup = new int[graph.VertexAmount()];
+        timer = 0;
     }
 
-    public static void dfs(int v, int p, ArrayList<ArrayList<Integer>> graph, ArrayList<Vertex> vertex_list, ArrayList<Edge> bridges_list) {
+    public static void Dfs(int v, int p, Graph graph, ArrayList<int[]> bridges_list) {
         used[v] = true;
         tin[v] = fup[v] = timer++;
-        for (int i = 0; i < graph.get(v).size(); ++i) {
-            Integer to = graph.get(v).get(i);
+        for (int i = 0; i < graph.GetVertexAdjacencyList(v).size(); ++i) {
+            Integer to = graph.GetVertexAdjacencyList(v).get(i);
             if (to == p)  continue;
-            if (used[to] == true)
+            if (used[to])
                 fup[v] = Math.min(fup[v], tin[to]);
             else {
-                dfs(to, v, graph, vertex_list, bridges_list);
+                Dfs(to, v, graph, bridges_list);
                 fup[v] = Math.min(fup[v], fup[to]);
                 if (fup[to] > tin[v]){
-                    for (Vertex v_start : vertex_list){
-                        for (Vertex v_end: vertex_list){
-                            if (v_start.num == v && v_end.num == to){
-                                bridges_list.add(new Edge(v_start, v_end));
-                            }
-                        }
-                    }
+                    bridges_list.add(new int[]{v, to});
                 }
             }
         }
     }
 
-    public static ArrayList<Edge> findBridges(ArrayList<Vertex> vertex_list, ArrayList<Edge> edges_list){
-        for (int i = 0; i < used.length; i++){
-            used[i] = false;
+    public ArrayList<int[]> FindBridges(Graph graph){
+        Arrays.fill(used, false);
+        ArrayList<int[]> bridges_list = new ArrayList<>();
+        for (int i = 0; i < graph.VertexAmount(); ++i){
+            if (!used[i]){
+                Dfs(i, -1, graph, bridges_list);
+            }
         }
-		ArrayList<ArrayList<Integer>> graph = converter(vertex_list, edges_list);
-        ArrayList<Edge> bridges_list = new ArrayList<>();
-        for (int i = 0; i < graph.size(); ++i)
-            if (used[i] == false)
-                dfs(i, -1, graph, vertex_list, bridges_list);
         return bridges_list;
     }
 }
