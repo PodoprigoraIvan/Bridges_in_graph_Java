7f834c6d25e3ce2e7f9451f4f6206af604200638
PodoprigoraIvan <shtarmel@gmail.com>
2022-07-06 19:26:17 +1000

    package add to application

--- src/main/java/com/github/podoprigoraivan/swingapp/Application.java
+++ src/main/java/com/github/podoprigoraivan/swingapp/Application.java
@@ -1,5 +1,6 @@
-import com.github.podoprigoraivan.swingapp.graphics.*;
+package com.github.podoprigoraivan.swingapp;
 
+import com.github.podoprigoraivan.swingapp.graphics.*;
 public class Application {
 	public static void main(String[] args) {
 		AppFrame frame = new AppFrame();
